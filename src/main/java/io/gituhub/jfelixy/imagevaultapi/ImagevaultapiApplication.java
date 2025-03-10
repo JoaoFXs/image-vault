5f2d41c1b3be03cfba88e64e4a201234d62a23b9
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-10 17:28:12 -0300

    first deploy

---
+++ src/main/java/io/gituhub/jfelixy/imagevaultapi/ImagevaultapiApplication.java
@@ -0,0 +1,13 @@
+package io.gituhub.jfelixy.imagevaultapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagevaultapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagevaultapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/gituhub/jfelixy/imagevaultapi/ImagevaultapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gituhub.jfelixy.imagevaultapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagevaultapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
