f8a19c316c675f96eff87d5b842eda0d80c4c9b8
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-15 12:20:59 -0300

    Add CommandLineRunner to save test image on startup
    Introduced a CommandLineRunner bean to save a sample image entity in the repository during application startup. This is useful for testing and verifying repository functionality.

--- src/main/java/io/gituhub/jfelixy/imagevaultapi/ImagevaultapiApplication.java
+++ src/main/java/io/gituhub/jfelixy/imagevaultapi/ImagevaultapiApplication.java
@@ -1,7 +1,13 @@
 package io.gituhub.jfelixy.imagevaultapi;
 
+import io.gituhub.jfelixy.imagevaultapi.domain.entity.Image;
+import io.gituhub.jfelixy.imagevaultapi.domain.enums.ImageExtension;
+import io.gituhub.jfelixy.imagevaultapi.infra.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -11,5 +17,17 @@ public class ImagevaultapiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImagevaultapiApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+		return args ->{
+			Image image = Image
+					.builder()
+					.extension(ImageExtension.PNG)
+					.name("myimage")
+					.tags("teste")
+					.size(1000L)
+					.build();
+			repository.save(image);
+		};
+	}
 }
