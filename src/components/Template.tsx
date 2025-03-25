07b24fd5cc595a1122885c06fc4fac78bfb57cd8
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-25 17:52:40 -0300

    Finish Header and Footer components

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -6,21 +6,33 @@ interface TemplateProps{
 export const Template: React.FC<TemplateProps> = (props: TemplateProps) => {
     return (
         <>
-            //header
             <Header/>
-            {props.children}
+            <div className="container mx-auto mt-8 px-4">
+                 {props.children}
 
-            //footer
+            </div>
+
+            <Footer/>
         </>
     )
 }
 
 const Header: React.FC = () => {
     return(
-        <header className="bg-indigo-950 text-white py-3">
+        <header className="bg-sky-700 text-white py-3">
             <div className="container mx-auto flex justify-between items-center px-4">
                 <h1 className="text-3xl font-bold">ImageVault</h1>
             </div>
         </header>
+    )
+}
+
+const Footer: React.FC = () => {
+    return(
+        <footer className="bg-sky-700 text-thite py-4 mt-8">
+            <div className="container mx-auto text-center">
+                Developed by João Felix
+            </div>
+        </footer>
     )
 }
