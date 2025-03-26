61b1e2a7794577c2bf1f343196142fe99bc4ca20
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-26 19:31:52 -0300

    Create renderImageCards from API

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -13,15 +13,24 @@ export default function GalleryPage(){
     async function searchImages(){
         const resultImages = await useService.search();
         setImages(resultImages);
-        console.table(images);
+       
+        // console.table(images);
     }
     
+    function renderImageCard(image: Image){
+        return (
+            <ImageCard name={image.name} src={image.url} size={image.size} dataUpload={image.uploadDate}/>
+        );
+    }
+
+    function renderImageCards(){
+        return images.map(renderImageCard)
+    }
     return(
         <Template>
             <button className='bg-gray-500' onClick={searchImages}>Click to change</button>
             <section className="grid grid-cols-4 gap-8">   
-                <ImageCard name="{imageName}" dataUpload='25/03/2025' size='10000kb' src=""/>
-            
+            {renderImageCards()}
             </section>
         </Template>
     )
