d94a98090598c86e1d1bedfbbc3de63ee8283455
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-31 18:20:22 -0300

    Include form

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -27,7 +27,20 @@ export default function GalleryPage(){
     }
     return(
         <Template>
-            <button className='bg-gray-500' onClick={searchImages}>Click to change</button>
+
+            <section className='flex flex-col items-center justify-center my-5'>
+                
+                <div className="flex space-x-4">
+                    <input type='text' className='border px-5 py-2 rounded-lg text-gray-700'/>
+                    <select className="border px-4 py-2 rounded-lg text-gray-900">
+                        <option>All formats</option>
+                    </select>
+                    <button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={searchImages}>Search</button>
+                    <button className="bg-yellow-500 text-white px-4 py-2 rounded-lg">Add New</button>
+                </div>
+
+            </section>
+
             <section className="grid grid-cols-4 gap-8">   
             {renderImageCards()}
             </section>
