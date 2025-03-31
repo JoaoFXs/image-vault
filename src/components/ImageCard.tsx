b9b73806737a292ca7967745867f8c379850f006
jovibfel@gmail.com <jovibfel@gmail.com>
2025-03-31 19:26:19 -0300

    Include download images function

--- src/components/ImageCard.tsx
+++ src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ImageCardProps{
     name?: string;
     size?: string;
@@ -6,9 +8,14 @@ interface ImageCardProps{
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({name, size, dataUpload, src}: ImageCardProps) => {
+    
+    function download(){
+        window.open(src, '_blank');
+    }
+    
     return (
         <div className="card relative bg-white rounded-md shadow-md transition-transform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
-            <img className="h-56 w-full object-cover rounded-t-md" alt="" src={src}/>
+            <img  onClick={download}  className="h-56 w-full object-cover rounded-t-md" alt="" src={src}/>
             <div className="card-body p-4">
                 <h5 className="text-xl font-semibold mb-2 text-gray-600">{name}</h5>
                 <p className="text-gray-600">{size}</p>
