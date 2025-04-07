b9ccfb9ceef3e9efc9c4caf8f0d6ac89e75d05af
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-07 18:23:26 -0300

    Create SecretKeyGenerator

--- src/main/java/io/gituhub/jfelixy/imagevaultapi/application/jwt/JwtService.java
+++ src/main/java/io/gituhub/jfelixy/imagevaultapi/application/jwt/JwtService.java
@@ -2,11 +2,18 @@ package io.gituhub.jfelixy.imagevaultapi.application.jwt;
 
 import io.gituhub.jfelixy.imagevaultapi.domain.AccessToken;
 import io.gituhub.jfelixy.imagevaultapi.domain.entity.User;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
     public AccessToken generateToken(User user){
+
+        String token = Jwts
+                        .builder()
+                        .signWith()
+                        .compact();
+
         return new AccessToken("");
     }
 }
