f2f7c2c2fe4ddd68fa1d4c50ed3cffcabec4b7a4
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-10 22:51:50 -0300

    Debug error conflict

--- src/resources/user/authentication.service.ts
+++ src/resources/user/authentication.service.ts
@@ -32,6 +32,8 @@ class AuthService{
         console.log("Response Auth.save: ", response);
 
         if(response.status == 409){
+            const error = await response.json();
+            console.log("Error: ", error);
             throw new Error('User alredy exists!');
         }
      
