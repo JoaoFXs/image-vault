8c008bcd7875e23e23c36796f2879208963db3a4
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-15 19:23:19 -0300

    Include auth session in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -23,6 +23,7 @@ export default function Login(){
             const credentials: Credentials = { email: values.email, password: values.password };
             try {   
                     const accessToken: AccessToken = await auth.authenticate(credentials);
+                    auth.initSession(accessToken);
                     router.push('/gallery');
                 
             } catch (error: any) {
