d4e90d03859320e14c5e62cfddcb0063788b7bef
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-16 23:05:01 -0300

    Update JwtFilter.java

--- src/main/java/io/gituhub/jfelixy/imagevaultapi/config/filter/JwtFilter.java
+++ src/main/java/io/gituhub/jfelixy/imagevaultapi/config/filter/JwtFilter.java
@@ -1,14 +1,28 @@
 package io.gituhub.jfelixy.imagevaultapi.config.filter;
 
+import io.gituhub.jfelixy.imagevaultapi.application.jwt.InvalidTokenException;
+import io.gituhub.jfelixy.imagevaultapi.application.jwt.JwtService;
+import io.gituhub.jfelixy.imagevaultapi.domain.entity.User;
+import io.gituhub.jfelixy.imagevaultapi.domain.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter{
+    private final JwtService jwtService;
+    private final UserService userService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
@@ -18,10 +32,31 @@ public class JwtFilter extends OncePerRequestFilter{
         String token = getToken(request);
 
         if(token != null){
-
+            try {
+                String email = jwtService.getEmailFromToken(token);
+                User user = userService.getByEmail(email);
+                setUserAsAuthenticated(user);
+            }catch (InvalidTokenException e){
+                log.error("Invalid Token: {}", e.getMessage());
+            }catch (Exception e){
+                log.error("Error validating the Token: {}", e.getMessage());
+            }
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setUserAsAuthenticated(User user){
+        UserDetails userDetails = org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles("USER")
+                .build();
+
+        UsernamePasswordAuthenticationToken authentication
+                = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     private String getToken(HttpServletRequest request){
