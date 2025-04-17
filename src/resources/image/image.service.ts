a0dad810e5098f47a80f1bcf5a36e1614fc2246e
jovibfel@gmail.com <jovibfel@gmail.com>
2025-04-17 19:02:14 -0300

    Include getUserSession in image.service

--- src/resources/image/image.service.ts
+++ src/resources/image/image.service.ts
@@ -1,10 +1,14 @@
 import { Image } from './image.resource'
+import { useAuth } from '@/resources'
 
 
 class ImageService{
     baseURL: string = 'http://localhost:8080/v1/images';
+    auth = useAuth();
 
     async search(query: string = "", extension: string = "") : Promise<Image[]> {
+        const userSession = this.auth.getUserSession();
+        
         const url = `${this.baseURL}?query=${query}&extension=${extension}` 
         const response = await fetch(url);
         return await response.json();
